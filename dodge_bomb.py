a6b48aa84745f420e9e9158e4293de7d8f87cf98
Futa <c0a2305018@edu.teu.ac.jp>
2024-04-23 15:13:59 +0900

    練習３：はみ出し判定

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -4,7 +4,7 @@ import sys
 import pygame as pg
 
 
-WIDTH, HEIGHT = 1600, 900
+WIDTH, HEIGHT = 1000, 600 
 DELTA = {  # 移動量辞書　（押下キーなど）
     pg.K_UP: (0, -5),
     pg.K_DOWN: (0, +5),
@@ -14,19 +14,35 @@ DELTA = {  # 移動量辞書　（押下キーなど）
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
+def check_bound(obj_rct):
+    """
+    こうかとんRect,または,爆弾Rectの画面内外判定用の関数
+    引数：こうかとんRect、または、爆弾Rect
+    戻り値：横方向判定結果、縦方向判定結果（True:画面内/False:画面外）
+    """
+    yoko, tate = True, True
+    if obj_rct.left < 0 or WIDTH < obj_rct.right:
+        yoko = False
+    if obj_rct.top < 0 or HEIGHT < obj_rct.bottom:
+        tate = False
+    return yoko, tate
+
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
+    #こうかとん設定
     bg_img = pg.image.load("fig/pg_bg.jpg")    
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 2.0)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 900, 400
+    #爆弾設定
     bd_img = pg.Surface((20, 20))
     bd_img.set_colorkey((0, 0, 0))
     pg.draw.circle(bd_img, (255, 0, 0), (10, 10), 10)
     bd_rct = bd_img.get_rect()
     bd_rct.center = random.randint(0,WIDTH), random.randint(0, HEIGHT)
-    vx, vy = +5, +5
+    vx, vy = +5, +5 #横方向速度、縦方向速度
     
     clock = pg.time.Clock()
     tmr = 0
@@ -35,7 +51,8 @@ def main():
             if event.type == pg.QUIT: 
                 return
         screen.blit(bg_img, [0, 0]) 
-
+        
+        #こうかとん移動
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
         for k, v in DELTA.items():
@@ -43,9 +60,17 @@ def main():
                 sum_mv[0] += v[0]
                 sum_mv[1] += v[1]
         kk_rct.move_ip(sum_mv)
+        if check_bound(kk_rct) != (True, True):
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
         screen.blit(kk_img, kk_rct)
+        #爆弾移動
         bd_rct.move_ip(vx, vy)
         screen.blit(bd_img, bd_rct)
+        yoko, tate = check_bound(bd_rct)
+        if not yoko:
+            vx *= -1
+        if not tate:
+            vy *= -1
         pg.display.update()
         tmr += 1
         clock.tick(50)
